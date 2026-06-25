4290c2b96ad4e679c26f6f2b4ea7a5fdc7a7a9bb
usmc6123 <usmc6123@gmail.com>
2026-06-25 05:24:11 -0500

    fix: update branding logo image URL
    Update the LOGO_URL to point to the correct workshop-specific image asset.

--- src/constants/branding.ts
+++ src/constants/branding.ts
@@ -1,2 +1,2 @@
-export const LOGO_URL = "https://raw.githubusercontent.com/usmc6123/images/main/3dlogo.jpg";
+export const LOGO_URL = "https://github.com/usmc6123/images/blob/main/3dlogoworkshop.jpg?raw=true";
 export const BACKGROUND_URL = "https://raw.githubusercontent.com/usmc6123/images/main/background.jpg";
