71a21284257530c183ecec9589f773f38d050718
usmc6123 <usmc6123@gmail.com>
2026-06-27 23:36:29 -0700

    chore(branding): update logo URL to new asset path

--- src/constants/branding.ts
+++ src/constants/branding.ts
@@ -1,2 +1,2 @@
-export const LOGO_URL = "https://github.com/usmc6123/images/blob/main/3dlogoworkshop.jpg?raw=true";
+export const LOGO_URL = "https://raw.githubusercontent.com/usmc6123/images/main/newlogo.jpg";
 export const BACKGROUND_URL = "https://github.com/usmc6123/images/blob/main/background.jpg?raw=true";
